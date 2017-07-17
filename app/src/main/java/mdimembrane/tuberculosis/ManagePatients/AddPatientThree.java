c9d19e44b0d0a202bb32c8fd9d27fca6cc4cf352
shivanshu <shivanshugoel96@gmail.com>
2017-07-17 16:59:33 +0530

    hello

--- app/src/main/java/mdimembrane/tuberculosis/ManagePatients/AddPatientThree.java
+++ app/src/main/java/mdimembrane/tuberculosis/ManagePatients/AddPatientThree.java
@@ -1,18 +1,25 @@
 package mdimembrane.tuberculosis.ManagePatients;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import mdimembrane.tuberculosis.main.PreferencesConstants;
 import mdimembrane.tuberculosis.main.R;
 
 public class AddPatientThree extends AppCompatActivity {
 
     CheckBox coughCB,hemoptysisCB,feverCB,weightLoseCB,wheezingCB,nightSweatsCB,fatigueCB,lymphadenpopathyCB,headacheCB,jointPainCB;
+    EditText othersymptomsET;
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,8 @@ public class AddPatientThree extends AppCompatActivity {
             Log.e("SearchActivity Toolbar", "You have got a NULL POINTER EXCEPTION");
         }
 
+        sharedpreferences = getSharedPreferences(PreferencesConstants.APP_MAIN_PREF, Context.MODE_PRIVATE);
+
         coughCB=(CheckBox)findViewById(R.id.coughCheckBox);
         hemoptysisCB=(CheckBox)findViewById(R.id.hemoptysisCheckBox);
         feverCB=(CheckBox)findViewById(R.id.feverCheckBox);
@@ -37,11 +46,47 @@ public class AddPatientThree extends AppCompatActivity {
         headacheCB=(CheckBox)findViewById(R.id.headacheCheckBox);
         jointPainCB=(CheckBox)findViewById(R.id.jointPainCheckBox);
 
+        othersymptomsET=(EditText)findViewById(R.id.symptomsEditText);
+
     }
 
     public void nextButton(View view)
     {
+        StringBuilder result=new StringBuilder();
+        result.append("Selected Symptoms:\n");
+        if(coughCB.isChecked()){
+            result.append("\nCough/Sputum > 3 Weeks ");
+        }
+        if(hemoptysisCB.isChecked()){
+            result.append("\nHemoptysis ");
+        }
+        if(feverCB.isChecked()){
+            result.append("\nFever ");
+        }
+        if(weightLoseCB.isChecked()){
+            result.append("\nWeight Lose / Anorexia ");
+        }
+        if(wheezingCB.isChecked()){
+            result.append("\nWheezing/SOB ");
+        }
+        if(nightSweatsCB.isChecked()){
+            result.append("\nNight Sweats ");
+        }
+        if(fatigueCB.isChecked()){
+            result.append("\nFatigue ");
+        }
+        if(lymphadenpopathyCB.isChecked()){
+            result.append("\nLymphadenpopathy ");
+        }
+        if(headacheCB.isChecked()){
+            result.append("\nHeadache ");
+        }
+        if(jointPainCB.isChecked()){
+            result.append("\nJoint Pain ");
+        }
 
+
+        Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
 
         Intent intent=new Intent(getApplicationContext(),AddPatientFour.class);
         startActivity(intent);
