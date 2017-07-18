3c583539635b16c23af04912c705edc68d98b149
shivanshu <shivanshugoel96@gmail.com>
2017-07-18 17:00:05 +0530

    hello

--- app/src/main/java/mdimembrane/tuberculosis/main/LoginActivity.java
+++ app/src/main/java/mdimembrane/tuberculosis/main/LoginActivity.java
@@ -206,7 +206,6 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         return password.length() > 4;
